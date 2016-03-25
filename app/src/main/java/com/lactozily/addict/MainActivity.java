c4a000bb55edd65849e22a13770ed15ffa4ba1b5
lactozily <lactoz.ily@gmail.com>
2016-03-25 13:59:08 +0700

    add_btn

--- app/src/main/java/com/lactozily/addict/MainActivity.java
+++ app/src/main/java/com/lactozily/addict/MainActivity.java
@@ -14,9 +14,13 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.evernote.android.job.JobManager;
 import com.evernote.android.job.JobRequest;
@@ -77,6 +81,27 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(TAG, "Service running");
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Toast.makeText(this, "HI", Toast.LENGTH_LONG).show();
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 
     @Override
     protected void onResume() {
